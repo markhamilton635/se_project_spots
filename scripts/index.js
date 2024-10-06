6edb7fd838650cd373c3be6f7a931e83a5568b0a
MARK HAMILTON <hamilton.mark37@gmail.com>
2024-10-05 17:33:01 -0700

    add modal user-interactivity

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,20 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editModal = document.querySelector("#edit-modal");
+
+function openModal() {
+  editModal.classList.add("modal__opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+const modalCloseButton = editModal.querySelector(".modal__close-btn");
+
+function closeModal() {
+  editModal.classList.remove("modal__opened");
+}
+
+modalCloseButton.addEventListener("click", closeModal);
