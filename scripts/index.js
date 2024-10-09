40040c555db84c955eb5c9d0738edc9ccae5a4bb
MARK HAMILTON <hamilton.mark37@gmail.com>
2024-10-09 13:12:25 -0700

    change openmodal to a modifier

--- scripts/index.js
+++ scripts/index.js
@@ -54,11 +54,11 @@ function getCardElement(data) {
 function openModal() {
   editModalNameInput.value = profileName.textContent;
   editModalDescriptionInput.value = profileDescription.textContent;
-  editModal.classList.add("modal__opened");
+  editModal.classList.add("modal_opened");
 }
 
 function closeModal() {
-  editModal.classList.remove("modal__opened");
+  editModal.classList.remove("modal_opened");
 }
 function handleEditFormSubmit(evt) {
   evt.preventDefault();
