ec556cbfeb728858c726223c465050f619f78443
MARK HAMILTON <hamilton.mark37@gmail.com>
2024-10-21 13:55:16 -0700

    refactor: for to foreach cards loop

--- scripts/index.js
+++ scripts/index.js
@@ -71,7 +71,7 @@ profileEditButton.addEventListener("click", openModal);
 modalCloseButton.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
 
-for (let i = 0; i < initialCards.length; i++) {
-  const cardElement = getCardElement(initialCards[i]);
+initialCards.forEach((item) => {
+  const cardElement = getCardElement(item);
   cardsList.prepend(cardElement);
-}
+});
