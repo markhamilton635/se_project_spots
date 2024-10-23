e5307d35444e46dc5e1c1b02cbef07c60df67a3a
MARK HAMILTON <hamilton.mark37@gmail.com>
2024-10-22 17:43:55 -0700

    add smooth transitions and fix modal close button and make responsive

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,10 @@ const initialCards = [
     name: "Mountain house",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
+  {
+    name: "Golden Gate bridge",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
+  },
 ];
 // Profile elements
 const profileName = document.querySelector(".profile__name");
@@ -44,6 +48,12 @@ const editModalDescriptionInput = editModal.querySelector(
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = cardModal.querySelector(".modal__form");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
+const previewModal = document.querySelector("#preview-modal");
+const previewModalImageEl = previewModal.querySelector(".modal__image");
+const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
+const modalPreviewCloseButton = previewModal.querySelector(
+  ".modal__close-btn_type_preview"
+);
 
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
@@ -72,6 +82,13 @@ function getCardElement(data) {
 
   cardLikeBtn.addEventListener("click", () => {
     cardLikeBtn.classList.toggle("card__like-btn_liked");
+  });
+
+  cardImageEl.addEventListener("click", () => {
+    openModal(previewModal);
+    previewModalImageEl.src = data.link;
+    previewModalImageEl.alt = data.name;
+    previewModalCaptionEl.textContent = data.name;
   });
 
   return cardElement;
@@ -107,6 +124,9 @@ profileEditButton.addEventListener("click", () => {
   openModal(editModal);
 });
 
+modalPreviewCloseButton.addEventListener("click", () => {
+  closeModal(previewModal);
+});
 modalCloseButton.addEventListener("click", () => {
   closeModal(editModal);
 });
